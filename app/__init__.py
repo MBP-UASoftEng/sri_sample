901d5ea8cccccfc5d8859320117e19c523ce6b60
Harsha <sbande@email.uark.edu>
2015-09-29 00:31:52 -0500

    initial commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,9 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.heroku import Heroku
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/test'
+heroku = Heroku(app)
 db = SQLAlchemy(app)
 
 from app import views, models
