a1b2f9cad0d96771b5e000155cbc9ba5fd6a93df
Joey <Joey@Joeys-MacBook-Air.local>
2015-10-29 16:34:32 -0500

    In progress

--- app/models.py
+++ app/models.py
@@ -5,46 +5,45 @@ from sqlalchemy.orm import relationship
 
 class TenderEntry(db.Model):
 
-    __tablename__ = 'TenderEntry'
+    __tablename__ = 'tenderEntry'
 
     id = db.Column(db.Integer, primary_key = True)
-    transactionID = db.Column(db.Integer, ForeignKey ("Transaction.id"), index = True, nullable = True)
+    transaction_id = db.Column(db.Integer, ForeignKey("transaction.id"), index=True, nullable=True)
     tenderType = db.Column(db.String)
     amount = db.Column(db.Integer)
     timestamp = db.Column(db.String)
 
-    transaction = db.relationship('Transaction', backref='transaction')
-
-    def __init__ (self, transactionID, tenderType, amount):
-        self.transactionID = transactionID
+    def __init__ (self, transaction, tenderType, amount):
+        self.transaction_id = transaction
         self.tenderType = tenderType
         self.amount = amount
         self.timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
     # For testing
     def __repr__ (self):
-        return "transactionID Id: %s" % transactionID
+        return "tender entry Id: %s" % self.id
 
 
 class Transaction(db.Model):
 
-    __tablename__ = 'Transaction'
+    __tablename__ = 'transaction'
 
     id = db.Column(db.Integer, primary_key=True)
     # cashier_id = db.Column(db.Integer, ForeignKey("employee.id"), nullable=False)
     cashier_id = db.Column(db.Integer, nullable=True) # Just for tests. Should interact with the "employee" table, as the line above.
     amount = db.Column(db.Float(precision=2, asdecimal=False, decimal_return_scale=None), nullable=False)
     transaction_type = db.Column(db.String, nullable=False)
-    parent_id = db.Column(db.Integer, ForeignKey("Transaction.id"), nullable=True)
+    parent_id = db.Column(db.Integer, ForeignKey("transaction.id"), index=True, nullable=True)
     timestamp = db.Column(db.String, nullable=False)
     
-    parent = db.relationship('Transaction', backref='parent_transaction')
+    parent = db.relationship(lambda: Transaction, remote_side=id, backref='parent_transaction')
+    tender_entries = db.relationship(lambda: TenderEntry, remote_side=id, backref='tender_transaction')
 
-    def __init__(self, cashier_id, amount, transaction_type, parent_id):
+    def __init__(self, cashier_id, amount, transaction_type, parent):
         self.cashier_id = cashier_id
         self.amount = amount
         self.transaction_type = transaction_type
-        self.parent_id = parent_id
+        self.parent = parent
         self.timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
     def __repr__(self):
--- app/views.py
+++ app/views.py
@@ -6,28 +6,27 @@ import random
 @app.route('/')
 def hello():
 
-	transaction = Transaction(123, 10, "Sale", None)
+	transaction = Transaction(123, 10.00, "Sale", None)
 
 	#Add the created entry to the table
 	db.session.add(transaction)
 
 	#transactionID = 55
 	tenderType = "cash"
-	amount = 64.67
-	timestamp = "2/12/2015"
-
+	amount = 75
+ 
 	validTenderTypes = ["cash", "gift", "mastercard", "visa", "discover", "amex", "debit"]
 
 	if tenderType.lower() not in validTenderTypes:
 		return "Invalid tender type"
 
 	#Create an entry in the tenderEntry table
-	tenderEntry = TenderEntry(transaction, tenderType, amount)
+	tenderEntry = TenderEntry(transaction.id, tenderType, amount)
 	
 	#Add the created entry to the table
 	db.session.add(tenderEntry)
-
+	
 	#Save the changes in the database
 	db.session.commit()
 
-	return str(len(a))
+	return "ok"
