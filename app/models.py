6987bed2fc8c5731c9bded3156eb9f8c18778b5e
Joey <Joey@Joeys-MacBook-Air.local>
2015-11-05 17:57:09 -0600

    I think this is my finished model

--- app/models.py
+++ app/models.py
@@ -3,12 +3,12 @@ import datetime
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship
 
-class TenderEntry(db.Model):
+class TenderEntry (db.Model):
 
     __tablename__ = 'tenderEntry'
 
     id = db.Column(db.Integer, primary_key = True)
-    transaction_id = db.Column(db.Integer, ForeignKey("transaction.id"), index=True, nullable=True)
+    transaction_id = db.Column(db.Integer, ForeignKey("transaction.id"), index = True, nullable = True)
     tenderType = db.Column(db.String)
     amount = db.Column(db.Integer)
     timestamp = db.Column(db.String)
@@ -24,27 +24,56 @@ class TenderEntry(db.Model):
         return "tender entry Id: %s" % self.id
 
 
-class Transaction(db.Model):
+class Transaction (db.Model):
 
     __tablename__ = 'transaction'
 
-    id = db.Column(db.Integer, primary_key=True)
-    # cashier_id = db.Column(db.Integer, ForeignKey("employee.id"), nullable=False)
-    cashier_id = db.Column(db.Integer, nullable=True) # Just for tests. Should interact with the "employee" table, as the line above.
-    amount = db.Column(db.Float(precision=2, asdecimal=False, decimal_return_scale=None), nullable=False)
-    transaction_type = db.Column(db.String, nullable=False)
-    parent_id = db.Column(db.Integer, ForeignKey("transaction.id"), index=True, nullable=True)
-    timestamp = db.Column(db.String, nullable=False)
+    id = db.Column(db.Integer, primary_key = True)
+    cashier_id = db.Column(db.Integer, ForeignKey("employee.id"), nullable = False)
+    # cashier_id = db.Column(db.Integer, nullable = True) # Just for testing
+    amount = db.Column(db.Float(precision=2, asdecimal = False, decimal_return_scale = None), nullable = False)
+    transaction_type = db.Column(db.String, nullable = False)
+    parent_id = db.Column(db.Integer, ForeignKey("transaction.id"), index = True, nullable = True)
+    timestamp = db.Column(db.String, nullable = False)
     
-    parent = db.relationship(lambda: Transaction, remote_side=id, backref='parent_transaction')
-    tender_entries = db.relationship(lambda: TenderEntry, remote_side=id, backref='tender_transaction')
+    parent = db.relationship(lambda: Transaction, remote_side = id, backref = 'parent_transaction')
+    tender_entries  =  db.relationship(lambda: TenderEntry, remote_side = id, backref = 'tender_transaction')
 
-    def __init__(self, cashier_id, amount, transaction_type, parent):
+    def __init__ (self, cashier_id, amount, transaction_type, parent):
         self.cashier_id = cashier_id
         self.amount = amount
         self.transaction_type = transaction_type
         self.parent = parent
         self.timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-    def __repr__(self):
+    def __repr__ (self):
         return '<Transaction ID %r>' % self.id
+
+class Employee (db.Model):
+
+    __tablename__ = 'employee'
+
+    id = db.Column(db.Integer, primary_key = True)
+    first_name = db.Column(db.String, nullable = False)
+    last_name = db.Column(db.String, nullable = False)
+    employee_id = db.Column(db.Integer, unique = True, nullable = False)
+    active = db.Column(db.Boolean, nullable = False)
+    classification = db.Column(db.String, nullable = False)
+    password = db.Column(db.String, nullable = False)
+    timestamp = db.Column(db.String, nullable = False)
+    manager_id = db.Column(db.Integer, ForeignKey("employee.id"), index = True, nullable = True)
+
+    manager = relationship(lambda: Employee, remote_side = id, backref = 'underlings')
+
+    def __init__ (self, first_name, last_name, active, classification, password, employee_id, manager):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.active = active
+        self.password = password
+        self.classification = classification
+        self.employee_id = employee_id
+        self.timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        self.manager = manager
+
+    def __repr__ (self):
+        return '<First Name %r>' % self.first_name
--- app/views.py
+++ app/views.py
@@ -1,10 +1,10 @@
 from app import app, db
 from flask import Flask
-from .models import TenderEntry, Transaction 
+from .models import TenderEntry, Transaction, Employee 
 import random
 
 @app.route('/')
-def hello():
+def hello ():
 
 	transaction = Transaction(123, 10.00, "Sale", None)
 
@@ -29,4 +29,6 @@ def hello():
 	#Save the changes in the database
 	db.session.commit()
 
-	return "ok"
+	a = TenderEntry.query.filter_by(tenderType = "cash").all()
+
+	return str(len(a))
